610db6a3d4b4a8eb25c2559592ce31ffea5ca93f
Adithya Raihan Pratama <144696680+Adithyaraihan@users.noreply.github.com>
2025-10-12 13:52:10 +0700

    Update server.js  Menambahkan ger ke path frontend

--- backend/server.js
+++ backend/server.js
@@ -416,11 +416,9 @@ app.post("/api/midtrans-notification", async (req, res) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.send("GoOutdoor backend is running 🚀");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
-
-
 app.listen(port, "0.0.0.0", () => {
   console.log(`🚀 Server GoOutdoor berjalan di port ${port}`);
 });
